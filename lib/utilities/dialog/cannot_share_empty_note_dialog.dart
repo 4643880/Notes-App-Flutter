b4fc162de3f84c4c87828b41f475e511cf7179b6
Aizaz Haider <aizazhaider720@gmail.com>
2022-06-17 20:28:24 +0500

    Added Notes Sharing Functionality using share plus plugin

---
+++ lib/utilities/dialog/cannot_share_empty_note_dialog.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/utilities/dialog/generic_dialog.dart';
+
+Future<void> showCanNotShareEmptyNoteDialog(
+  BuildContext context,
+  String title,
+  String desc,
+) async {
+  return showGenericDialog(
+    context: context,
+    title: title,
+    content: desc,
+    optionsBuilder: () {
+      return {"OK": null};
+    },
+  );
+}
--- lib/views/note views/create_update_note_view.dart
+++ lib/views/note views/create_update_note_view.dart
@@ -3,9 +3,11 @@ import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/services/cloud/cloud_note.dart';
 import 'package:mynotes/services/cloud/cloud_storage_exceptions.dart';
 import 'package:mynotes/services/cloud/firebase_cloud_storage.dart';
+import 'package:mynotes/utilities/dialog/cannot_share_empty_note_dialog.dart';
 import 'dart:developer' as devtools show log;
 
 import 'package:mynotes/utilities/generics/get_arguments.dart';
+import 'package:share_plus/share_plus.dart';
 
 class CreateUpdateNoteView extends StatefulWidget {
   const CreateUpdateNoteView({Key? key}) : super(key: key);
@@ -94,6 +96,18 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Note"),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final text = _textController.text;
+              if(_note == null || text.isEmpty){
+                await showCanNotShareEmptyNoteDialog(context, "Sharing", "You can not share empty note.");
+              }else{
+                Share.share(text);
+              }
+            },
+            icon: const Icon(Icons.share),),
+        ],
       ),
       body: FutureBuilder(
         future: createOrGetExistingNote(context),
